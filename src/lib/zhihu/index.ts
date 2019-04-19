96e881fa1ae2a75f1ca2a93b19bdf6dc276fee9f
hewking <604910070@qq.com>
2019-04-19 18:57:18 +0800

    udpate

--- src/lib/zhihu/index.ts
+++ src/lib/zhihu/index.ts
@@ -1,21 +1,28 @@
 import Requests from "../../base/Requests";
 
-interface AccountInterface {
-    readonly a : string,
-    readonly p : string,
-}
-
 export default class ZhiHu extends Requests {
 
-    constructor(){
+    constructor(account : string,password : string){
         super()
         // login api
         this.url = 'https://www.zhihu.com/api/v3/oauth/sign_in'
+        this.account = account
+        this.password = password
     }
 
-    async signIn(account : string | [AccountInterface],password? : string) : Promise<void>{
-        this.account = account
-        this.password = password || ''
+    async signIn(captchaLang = 'en',loadCookies = true) : Promise<void>{
+
+        /**
+         *  模拟登录知乎
+        :param captcha_lang: 验证码类型 'en' or 'cn'
+        :param load_cookies: 是否读取上次保存的 Cookies
+        :return: bool
+        * 1. check cookies
+        2.
+         */
+
+
+        
 
         let headers = {
             'accept-encoding': 'gzip, deflate, br',
@@ -28,12 +35,17 @@ export default class ZhiHu extends Requests {
             'client_id': 'c3cef7c66a1843f8b3a9e6a1e3160e20',
             'grant_type': 'password',
             'source': 'com.zhihu.web',
-            'username': '',
-            'password': '',
-            'lang': 'en',
+            'username': this.account,
+            'password': this.password,
+            'lang': captchaLang,
             'ref_source': 'homepage',
             'utm_source': ''
         }
+
+        let timestramp = new Date().getTime()
+
+        // fromData.timestramp = timestramp
+
 
         return await this.rq({
             method:'POST',
