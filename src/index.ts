5ae4d4e3680f2f690252750a25960e179e253b6b
ZheLin <chengzhelin@foxmail.com>
2019-04-22 17:55:35 +0800

    删除

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,7 @@ export {
 
 if (require.main === module) {
   (async function () {
-    // let w = new Bilibili()
-    // let data: object = await w.start('13800138001', 'asdasd4565')
-    // console.log(data)
-    // let w = new Bilibili()
-    // let a = Math.floor(Math.random() * 100000000)
-    // 3827825
-    // await w.start('', '')
-    // console.log(w.getCookie())
-    // let data: object = await w.start('13800138000', 'asdasd4565')
-    // console.log(data)
+    let w = new QQVideo()
+    await w.start('', '')
   })()
 }
